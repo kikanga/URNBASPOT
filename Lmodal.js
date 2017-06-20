9d709649cfde015ca647fb5a1940bd522f5396e6
Chris Aja <owner2@Anas-MacBook-Pro.local>
2017-06-20 14:23:49 -0700

    login modal update

--- Lmodal.js
+++ Lmodal.js
@@ -26,11 +26,11 @@ class ModalExample extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Want to enjoy more of YepNBA? Login or create an account now.
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={this.toggle}>Login</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Close</Button>
+            <Button color="secondary" onClick={this.toggle}>Signup</Button>
           </ModalFooter>
         </Modal>
       </div>
@@ -38,4 +38,4 @@ class ModalExample extends React.Component {
   }
 }
 
-export default LModal;
+export default Lmodal;
